f2b5cb2301135aab5de28de5436a37f9e6257c05
NaveenGokavarapu19 <66323587+NaveenGokavarapu19@users.noreply.github.com>
2022-06-29 16:26:23 +0530

    Create AdditionOfTwoNumbers.java

---
+++ conditonals/AdditionOfTwoNumbers.java
@@ -0,0 +1,18 @@
+package com.conditionals;
+
+import java.util.Scanner;
+
+
+public class AdditionOfTwoNumbers
+{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter a: ");
+		int a = sc.nextInt();
+		System.out.print("Enter b: ");
+		int b = sc.nextInt();
+		int total_sum = a + b;
+		System.out.println(total_sum);
+	}
+	
+}
