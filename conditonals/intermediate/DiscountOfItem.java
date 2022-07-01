a64bf2c7e399ef88c0b74db22051b610b67efc3d
NaveenGokavarapu19 <66323587+NaveenGokavarapu19@users.noreply.github.com>
2022-07-01 11:57:06 +0530

    Changed the class name to correct class name

--- conditonals/intermediate/DiscountOfItem.java
+++ conditonals/intermediate/DiscountOfItem.java
@@ -2,7 +2,7 @@ package com.conditionals.intermediate;
 
 import java.util.Scanner;
 
-public class Main
+public class DiscountOfItem
 {
 	public static void main(String[] args) {
 	    Scanner sc = new Scanner(System.in);
