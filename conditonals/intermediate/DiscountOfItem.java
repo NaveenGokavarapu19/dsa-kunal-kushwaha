a35b6db211d13d94823e65a96b1071ec44ec9429
NaveenGokavarapu19 <66323587+NaveenGokavarapu19@users.noreply.github.com>
2022-07-01 09:48:31 +0530

    Create DiscountOfItem.java

---
+++ conditonals/intermediate/DiscountOfItem.java
@@ -0,0 +1,16 @@
+package com.conditionals.intermediate;
+
+import java.util.Scanner;
+
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+	    System.out.print("Enter the value of List Price: ");
+	    int list_price = sc.nextInt();
+	    System.out.print("Enter the value of Selling Price: ");
+	    int selling_price = sc.nextInt();
+	    int discount = list_price - selling_price;
+	    System.out.println(discount);
+	}
+}
