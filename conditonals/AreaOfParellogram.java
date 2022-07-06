d0b04d7764bcf9ea519853b2623e894a25dca990
NaveenGokavarapu19 <66323587+NaveenGokavarapu19@users.noreply.github.com>
2022-07-06 21:47:30 +0530

    Create AreaOfParellogram.java

---
+++ conditonals/AreaOfParellogram.java
@@ -0,0 +1,15 @@
+package com.conditonals;
+
+import java.util.Scanner;
+
+class  {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Input b: " );
+        double b = sc.nextDouble();
+        System.out.print("Input h: ");
+        double h = sc.nextDouble();
+        double area = b*h;
+        System.out.println(area); 
+    }
+}
