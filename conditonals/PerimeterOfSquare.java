6c60ffe10e726b0a031f0069f3ce3264e79454fd
NaveenGokavarapu19 <66323587+NaveenGokavarapu19@users.noreply.github.com>
2022-07-06 22:36:02 +0530

    Update and rename AreaOfSquare.java to PerimeterOfSquare.java

--- conditonals/PerimeterOfSquare.java
+++ conditonals/PerimeterOfSquare.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 import java.lang.Math;
 
 
-class  AreaOfSquare{
+class  PerimeterOfSquare{
        public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Input a: " );
