77f8c13a2902d2dd6e59c5159ea1dbbe53c97b7d
NaveenGokavarapu19 <66323587+NaveenGokavarapu19@users.noreply.github.com>
2022-07-06 22:33:09 +0530

    Create AreaOfSquare.java

---
+++ conditonals/AreaOfSquare.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+import java.lang.Math;
+
+
+class  AreaOfSquare{
+       public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Input a: " );
+        double a = sc.nextDouble();
+        double area = Math.pow(a,2);
+        System.out.printf("%.2f",area); 
+    }
+}
