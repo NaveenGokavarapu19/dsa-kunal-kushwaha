0cfa642b19eb91a33a6a511b68c58b075b9ae1d5
NaveenGokavarapu19 <66323587+NaveenGokavarapu19@users.noreply.github.com>
2022-07-07 10:42:11 +0530

    Create PerimeterOfParellogram.java

---
+++ conditonals/PerimeterOfParellogram.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+
+class PerimeterOfParellogram {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Input l: ");
+        double l = sc.nextDouble();;
+        System.out.print("Input b: ");
+        double b = sc.nextDouble();        
+        double perimeter = 2 * (l+b);
+        System.out.printf("%.2f",perimeter); 
+    }
+}
